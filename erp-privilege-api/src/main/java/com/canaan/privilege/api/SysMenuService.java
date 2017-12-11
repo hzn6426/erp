c3176669eb100b8ba7d597c2816c4bb21bd97e97
hzn6426 <hzn646426@163.com>
2017-12-11 13:44:11 +0800

    add config files

--- erp-privilege-api/src/main/java/com/canaan/privilege/api/SysMenuService.java
+++ erp-privilege-api/src/main/java/com/canaan/privilege/api/SysMenuService.java
@@ -4,6 +4,6 @@ package com.canaan.privilege.api;
 import com.canaan.common.BaseService;
 import com.canaan.privilege.dto.MenuDTO;
 
-public interface MenuService extends BaseService<MenuDTO> {
+public interface SysMenuService extends BaseService<MenuDTO> {
 
 }
--- erp-privilege/src/main/java/com/canaan/privilege/service/DefaultMenuService.java
+++ erp-privilege/src/main/java/com/canaan/privilege/service/DefaultMenuService.java
@@ -4,12 +4,12 @@ package com.canaan.privilege.service;
 import org.jooq.Condition;
 import org.jooq.SortField;
 
-import com.canaan.privilege.api.MenuService;
+import com.canaan.privilege.api.SysMenuService;
 import com.canaan.privilege.db.tables.SysMenu;
 import com.canaan.privilege.db.tables.records.SysMenuRecord;
 import com.canaan.privilege.dto.MenuDTO;
 
-public class DefaultMenuService extends DefaultBaseService<SysMenuRecord, SysMenu, MenuDTO> implements MenuService {
+public class DefaultMenuService extends DefaultBaseService<SysMenuRecord, SysMenu, MenuDTO> implements SysMenuService {
 
 	@Override
 	public Condition condition(MenuDTO e) {
