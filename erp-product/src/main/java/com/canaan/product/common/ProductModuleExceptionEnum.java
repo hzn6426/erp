3e9b84720402419a537b7306ebead28000ae4538
hzn6426 <hzn646426@163.com>
2018-01-03 17:42:14 +0800

    mybatis-plus添加

---
+++ erp-product/src/main/java/com/canaan/product/common/ProductModuleExceptionEnum.java
@@ -0,0 +1,21 @@
+package com.canaan.product.common;
+
+import com.canaan.core.exception.ExceptionEnumable;
+
+public enum ProductModuleExceptionEnum implements ExceptionEnumable {
+
+	EXCEPTION_SAMPLE(10020);
+	
+	ProductModuleExceptionEnum(final int code) {
+		this.code = code;
+	}
+	
+	private int code;
+	
+	@Override
+	public int getExceptionCode() {
+		return this.code;
+	}
+
+	
+}
